46aaa97e544efb0714983f725ed5ec029836a8b0
Kazeem Omoloja <omolojakazeem@gmail.com>
2020-04-20 23:01:24 +0100

    initial Commit

---
+++ psm_website/settings/prod.py
@@ -0,0 +1,35 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['.heroku.com']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'psm_mydb',
+        'USER': 'psm_myadmin',
+        'PASSWORD': 'Oluteofute1.',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
